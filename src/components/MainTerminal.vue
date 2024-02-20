d3a51db5b95b28f02ccbe34d8f0b926f3dc17802
dusdjhyeon <dusdj0813@gmail.com>
2024-02-20 15:10:56 +0900

    feat: create terminal frame

---
+++ src/components/MainTerminal.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="terminal">
+        <div id="output"></div>
+        <div id="prompt">
+            <span id="username">dusdjhyeon></span>
+            <input id="input" type="text">
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+@import '@/styles/terminal.css';
+</style>
