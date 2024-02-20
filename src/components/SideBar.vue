4cd5f896409971e029ba6552c6486a67bbc907a4
dusdjhyeon <dusdj0813@gmail.com>
2024-02-20 15:11:08 +0900

    feat: create sidebar frame

---
+++ src/components/SideBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="sidebar" class="sidebar">
+        <h3>Cluster: minikube</h3>
+        <h4>Namespaces</h4>
+        <div id="namespace-list"></div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+@import '@/styles/sidebar.css';
+</style>
