0d77af203979e885b622d2111eecbf866ec11bb9
dusdjhyeon <dusdj0813@gmail.com>
2024-02-21 16:42:40 +0900

    feat: pods list in namespace

--- src/components/NamespaceBlock.vue
+++ src/components/NamespaceBlock.vue
@@ -1,13 +1,22 @@
 <template>
     <div>
-        <div v-for="(item, index) in list" 
-        :key="index" 
-        :class="{ 'box-clicked': clickedIndex === index }"
-        class="box"
-        @click="selectBox(index)"
-        >
-            {{  item  }}
-        </div>
+        <div v-for="(item, index) in list" :key="index">
+            <button 
+            :class="{ 'box-clicked': clickedIndex === index }"
+            class="box"
+            @click="selectBox(index)"
+            >
+                {{  item  }}
+            </button>
+            <div v-if="clickedIndex === index" class="pods-list">
+                <div v-for="(status, podname) in podData" :key="podname" class="pod-info">
+                    <div class="pod-wrapper">
+                        {{ podname }}
+                    </div>
+                    <span :class="{ 'running': status === 'Running', 'not-running': status !== 'Running' }"></span>
+                </div>
+            </div>
+        </div>       
     </div>
 </template>
 
@@ -21,12 +30,21 @@ export default {
     },
     data() {
         return {
-            clickedIndex: null
+            clickedIndex: null,
+            podData: {
+                "pod1": "Running",
+                "pod2": "Pending",
+                "pod3": "Failed",
+            }
         }
     },
     methods: {
         selectBox(index) {
-            this.clickedIndex = index;
+            if (this.clickedIndex === index) {
+                this.clickedIndex = null;
+            } else {
+                this.clickedIndex = index;
+            }
         }
     }
 }
@@ -34,15 +52,52 @@ export default {
 
 <style scoped>
 .box {
-  height: 37px;  
-  display: flex; 
-  align-items: center; 
-  justify-content: center; 
-  margin-bottom: 0px; 
-  width: 100%;
-  font-size:14px;
+    border: 0;
+    outline: 0;
+    height: 40px;  
+    display: flex; 
+    align-items: center; 
+    justify-content: center; 
+    padding: 10px;
+    margin: 0px;
+    width: 100%;
+    font-size:14px;
+    font-weight: bold;
+    color:beige;
+    background-color: transparent;
+    flex-direction: column;
 }
 .box-clicked {
     background-color:rgb(121, 113, 103);
+}
+.pods-list{
+    border: 0;
+    outline: 0;
+    height: flex;
+    align-items: center;
+    justify-content: center;
+}
+.pod-info {
+    display: flex;
+    align-items: center;
+    font-size: 13px;
+    justify-content: center;
+}
+.pod-wrapper {
+    margin: 5px;
+}
+.running {
+    width: 10px;
+    height: 10px;
+    background-color: rgb(5, 155, 5);
+    border-radius: 50%;
+    margin-left: 10px;
+}
+.not-running {
+    width: 10px;
+    height: 10px;
+    background-color: red;
+    border-radius: 50%;
+    margin-left: 10px;
 }
 </style>
