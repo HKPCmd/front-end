84096f18debbcf432c4ffdd6e5dd7a14abc809f0
dusdjhyeon <dusdj0813@gmail.com>
2024-02-23 10:50:12 +0900

    feat: link api

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios' 
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = 'http://172.25.219.226:8000'
+
+Vue.prototype.$axios = axios
 
 new Vue({
   render: h => h(App),
